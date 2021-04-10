c42cb8a55385998cd06672ee90e33c6518fcfa2f
Evan Mills <evanmills113@gmail.com>
2021-04-10 01:05:33 -0500

    general travel working

--- bot/Nat20.py
+++ bot/Nat20.py
@@ -6,13 +6,20 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+
+PREFIX='!'
+
+f = open('commands.md')
+command_message = f.read()
+
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-client = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix=PREFIX)
 
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Game(name='!commands to list possible commands'))
     print('Online')
 
 
@@ -58,6 +65,11 @@ async def roll(ctx):
 
 
 @client.command()
+async def commands(ctx):
+    await ctx.message.channel.send(command_message)
+
+
+@client.command()
 async def register(ctx):
     """
     Command used to register a character
@@ -68,18 +80,86 @@ async def register(ctx):
 
 
 @client.command()
+async def locations(ctx):
+    """
+    Command to list out the locations in the world
+    :param ctx: context for the command
+    :return: None
+    """
+    channel = ctx.message.channel
+    await channel.send('Locations')
+
+
+@client.command()
 async def travel(ctx):
-    pass
+    author = ctx.message.author
+    print(author)
+    message = ctx.message.content
+
+    location = message.replace('!travel ', '')
+
+    roles = ctx.message.guild.roles
+
+    location_role = None
+    for temp in roles:
+        if temp.name == location:
+            location_role = temp
+            break
+
+    if location_role is None:
+        await create_role(ctx, location)
+        await create_role(ctx, f'{location}-general')
+        await create_channel(ctx, f'{location}-general')
+    else:
+        general_role = None
+        for temp in roles:
+            if temp.name == f'{location}-general':
+                general_role = temp
+                break
+
+        if general_role is None:
+            create_role(ctx, f'{location}-general')
+            await create_channel(ctx, f'{location}-general')
+        else:
+            await ctx.message.author.add_roles(general_role)
+
+        await ctx.message.author.add_roles(location_role)
 
 
 @client.command()
 async def enter(ctx):
     pass
 
-async def create_channel(channel_name):
-    pass
 
-async def create_role(role_name):
-    pass
+async def create_channel(ctx, channel_name):
+    """
+    Function to create and manage a new channel
+    :param ctx: context of channel create
+    :param channel_name: name for the channel to be created
+    :return: None
+    """
+    roles = ctx.message.guild.roles
+    role = None
+    for temp in roles:
+        if temp.name == channel_name:
+            role = temp
+            break
+
+    if role is None:
+        print('Role not found')
+        return
+
+    print(role)
+    overwrites = {
+        ctx.message.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        ctx.message.guild.me: discord.PermissionOverwrite(read_messages=True),
+        role: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+    }
+    await ctx.message.guild.create_text_channel(channel_name, overwrites=overwrites)
+
+
+async def create_role(ctx, role_name):
+    role = await ctx.message.guild.create_role(name=role_name)
+    await ctx.message.author.add_roles(role)
 
 client.run(TOKEN)
---
+++ bot/commands.md
@@ -0,0 +1,7 @@
+My prefix is `!`
+> commands: Lists the commands
+> travel: Move you character between major location
+> enter: enter into minor locations
+> roll: roll dice (d20, 2xd10)
+> register: register a new character
+> location: lists out the locations in the world
